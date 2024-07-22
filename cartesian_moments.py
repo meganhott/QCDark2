867e97f1b160067a29385ae88ade643b8a617610
meganhott <46502432+meganhott@users.noreply.github.com>
2024-07-22 16:41:46 -0400

    Fixed indentation issues

--- cartesian_moments.py
+++ cartesian_moments.py
@@ -7,77 +7,85 @@ e = math.e
 etol = 10.**8
 
 def fact2(n):
-     if n <= 1:
-          return 1.
-     else:
-          return factorial2(n)
-
+    if n <= 1:
+        return 1.
+    else:
+        return factorial2(n)
+    
 def get_E_ijt(a: float, b: float, i_max: int, j_max: int, Qx: float) -> list[list[list[float]]]:
-     """
-     Recursively calculate Hermite Gaussian Coefficients for 1d primitive gaussians.
-     Inputs:
-          a, b           = exponents of 1d primitive Gaussian 'a' and 'b' respectively
-          i_max, j_max   = maximum angular momentum number on 1d primitive Gaussian 'a' and 'b' respectively
-          Qx             = distance between origins of 1d primitive Gaussian 'a' and 'b', Qx = Ax - Bx
-     Returns:
-          list[list[list[float]]]: E_{ij}^t for a, b, Qx
-     """
+    """
+    Recursively calculate Hermite Gaussian Coefficients for 1d primitive gaussians.
+    Inputs:
+        a, b           = exponents of 1d primitive Gaussian 'a' and 'b' respectively
+        i_max, j_max   = maximum angular momentum number on 1d primitive Gaussian 'a' and 'b' respectively
+        Qx             = distance between origins of 1d primitive Gaussian 'a' and 'b', Qx = Ax - Bx
+    Returns:
+        list[list[list[float]]]: E_{ij}^t for a, b, Qx
+    """
      
-     p = a + b
-     q = a*b/p
+    p = a + b
+    q = a*b/p
 
-     if i_max < 0 or j_max < 0:
-          raise ValueError('i and j must be greater than 0. Given i = {}, j = {}'.format(i_max, j_max))
+    if i_max < 0 or j_max < 0:
+        raise ValueError('i and j must be greater than 0. Given i = {}, j = {}'.format(i_max, j_max))
 
-     E_ijt = []
-     for i in range(i_max + 1):
-          tem1 = []
-          for j in range(j_max+1):
-               tem1.append([None]*(i+j+1))
-          E_ijt.append(tem1)
+    E_ijt = []
+    for i in range(i_max + 1):
+        tem1 = []
+        for j in range(j_max+1):
+            tem1.append([None]*(i+j+1))
+        E_ijt.append(tem1)
 
-     def E(a: float, b: float, i: int, j: int, t: int, Qx: float, p: float, q: float) -> float:
-          """
-          Recursively calculate Hermite Gaussian Coefficients for 1d primitive gaussians.
-          Inputs:
-               a, b = exponents of 1d primitive Gaussian 'a' and 'b' respectively
-               i, j = angular momentum number on 1d primitive Gaussian 'a' and 'b' respectively
-               t    = number of nodes in the Hermite
-               Qx   = distance between origins of 1d primitive Gaussian 'a' and 'b', Qx = Ax - Bx
-               p, q = derived quantities from a and b used frequently. Speeds up calculation.
-          Returns:
-               float: E_{ij}^t for a, b, Qx
-          """
-          nonlocal E_ijt
+    def E(a: float, b: float, i: int, j: int, t: int, Qx: float, p: float, q: float) -> float:
+        """
+        Recursively calculate Hermite Gaussian Coefficients for 1d primitive gaussians.
+        Inputs:
+            a, b = exponents of 1d primitive Gaussian 'a' and 'b' respectively
+            i, j = angular momentum number on 1d primitive Gaussian 'a' and 'b' respectively
+            t    = number of nodes in the Hermite
+            Qx   = distance between origins of 1d primitive Gaussian 'a' and 'b', Qx = Ax - Bx
+            p, q = derived quantities from a and b used frequently. Speeds up calculation.
+        Returns:
+            float: E_{ij}^t for a, b, Qx
+        """
+        nonlocal E_ijt
 
-          if (t < 0) or (t > i+j):                                    # 0 < t < i + j
-               return 0.
-          if not E_ijt[i][j][t]:
-               if i == j == t == 0:                                      # E_{00}^0 = K_{AB} = e^{-q Qx^2}
-                    E_ijt[i][j][t] = e**(-q*Qx*Qx)
-               elif j == 0:                                                # Decrement index i
-                    E_ijt[i][j][t] =    1./(2.*p)*E(a, b, i-1, j, t-1, Qx, p, q) - \
-                                        q*Qx/a*E(a, b, i-1, j, t, Qx, p, q)      + \
-                                        (t+1.)*E(a, b, i-1, j, t+1, Qx, p, q)
-               else:                                                       # Default decrement index j
-                    E_ijt[i][j][t] =    1./(2.*p)*E(a, b, i, j-1, t-1, Qx, p, q) + \
-                                        q*Qx/b*E(a, b, i, j-1, t, Qx, p, q)      + \
-                                        (t+1.)*E(a, b, i, j-1, t+1, Qx, p, q)
-          return E_ijt[i][j][t]
+        if (t < 0) or (t > i+j):                                    # 0 < t < i + j
+            return 0.
+        if not E_ijt[i][j][t]:
+            if i == j == t == 0:                                      # E_{00}^0 = K_{AB} = e^{-q Qx^2}
+                E_ijt[i][j][t] = e**(-q*Qx*Qx)
+            elif j == 0:                                                # Decrement index i
+                E_ijt[i][j][t] =    1./(2.*p)*E(a, b, i-1, j, t-1, Qx, p, q) - \
+                                    q*Qx/a*E(a, b, i-1, j, t, Qx, p, q)      + \
+                                    (t+1.)*E(a, b, i-1, j, t+1, Qx, p, q)
+            else:                                                       # Default decrement index j
+                E_ijt[i][j][t] =    1./(2.*p)*E(a, b, i, j-1, t-1, Qx, p, q) + \
+                                    q*Qx/b*E(a, b, i, j-1, t, Qx, p, q)      + \
+                                    (t+1.)*E(a, b, i, j-1, t+1, Qx, p, q)
+        return E_ijt[i][j][t]
      
 
-     for i in range(i_max + 1):
-          for j in range(j_max+1):
-               for t in range(i + j + 1):
-                    E_ijt[i][j][t] = E(a, b, i, j, t, Qx, p, q)
+    for i in range(i_max + 1):
+        for j in range(j_max+1):
+            for t in range(i + j + 1):
+                E_ijt[i][j][t] = E(a, b, i, j, t, Qx, p, q)
      
-     return E_ijt
+    return E_ijt
 
 def primgauss_1D_overlaps_uR(R: float, primindices: np.ndarray, q: np.ndarray, atom_locs: np.ndarray) -> np.ndarray:
-    TN = int(round(primindices[-1, -1] + primindices[-1, 1] + 1))
+    """
+    All inputs are in one dimension
+
+    R:             np.ndarray of shape (N_unique_Rd,): All unique d-components of R vectors
+    primindices:   np.ndarray of shape (N_shells, 4)
+    q:             np.ndarray of shape (N_unique_qd): All unique d-components of q vectors
+    atom_locs:     np.ndarray of shape (N_atoms,): d-components of atomic locations
+    """
+    TN = int(round(primindices[-1, -1] + primindices[-1, 1] + 1)) #total number of primitive gaussians
     res = np.zeros((TN, TN, len(q))).astype('complex128')
     for p1 in primindices:
-        a1, l1, e1, I1 = int(p1[0]), int(p1[1]), p1[2], int(p1[3])
+        a1, l1, e1, I1 = int(p1[0]), int(p1[1]), p1[2], int(p1[3]) #atom id, max l, exponent, first index in primgauss
         A = atom_locs[a1]
         for p2 in primindices:
             a2, l2, e2, I2 = int(p2[0]), int(p2[1]), p2[2], int(p2[3])
@@ -93,107 +101,106 @@ def primgauss_1D_overlaps_uR(R: float, primindices: np.ndarray, q: np.ndarray, a
     return res
 
 class AO(object):
-     """
-     Object that defines an atomic orbital. 
-     Attributes:
-          atom:          int, index of atom
-          loc:           np.ndarray of shape (3,), contains origin of atom
-          shell:         tuple of len = 3, angular momentum components for all 3 directions
-          Nprim:         int, number of primitive gaussians, N_prim
-          exp:           np.ndarray of shape (N_prim, )
-                         contains all exponents in the contracted gaussian
-          coef:          np.ndarray of shape (N_prim, )
-                         contains all coefficients in the contracted gaussian
-          norm:          np.ndarray of shape (N_prim, )
-                         contains all normalizations in the contracted gaussian
-          prim_indices:  np.ndarray of shape (3, N_prim)
-                         contains indices of primitive gaussian in 1D corresponding to 
-                         angular momentum component in each direction and each exponent
-                         as contained in primgauss np.ndarray.
-     Requires following input parameters to initialize:
-          atom_index:    int, index of atom being considered
-          ijk:           tuple of len = 3, angular momentum components for all 3 directions
-          exps:          np.ndarray of shape (N_prim, )
-                         contains all exponents in the contracted gaussian
-          coeffs:        np.ndarray of shape (N_prim, )
-                         contains all coefficients in the contracted gaussian
-          primgauss:     np.ndarray of shape (tot_prim, 7)
-                         contains details of all primgauss elements.
+    """
+    Object that defines an atomic orbital. 
+    Attributes:
+        atom:           int, index of atom
+        loc:            np.ndarray of shape (3,), contains origin of atom
+        shell:          tuple of len = 3, angular momentum components for all 3 directions
+        Nprim:          int, number of primitive gaussians, N_prim
+        exp:            np.ndarray of shape (N_prim, )
+                        contains all exponents in the contracted gaussian
+        coef:           np.ndarray of shape (N_prim, )
+                        contains all coefficients in the contracted gaussian
+        norm:           np.ndarray of shape (N_prim, )
+                        contains all normalizations in the contracted gaussian
+        prim_indices:   np.ndarray of shape (3, N_prim)
+                        contains indices of primitive gaussian in 1D corresponding to 
+                        angular momentum component in each direction and each exponent
+                        as contained in primgauss np.ndarray.
+    Requires following input parameters to initialize:
+        atom_index:     int, index of atom being considered
+        ijk:            tuple of len = 3, angular momentum components for all 3 directions
+        exps:           np.ndarray of shape (N_prim, )
+                        contains all exponents in the contracted gaussian
+        coeffs:         np.ndarray of shape (N_prim, )
+                        contains all coefficients in the contracted gaussian
+        primgauss:      np.ndarray of shape (tot_prim, 7)
+                        contains details of all primgauss elements.
      
-     Q: can it be sped up? Can we eliminate the need to read in primgauss object?
-     Notes:    
-          1.   It is easy to pass the location array.
-          2.   For any exp in self.exp and i in self.shell, we can use exp and i as keys in dictionary.
-          3.   This would have to change the goal, and we will have to list atom_id = self.atom as a key
-               to be passed to the overlap dictionary. This is because of the following:
-               self.prim_indices contains information on the location already. 
-     """
-     def __init__(self, atom_index: int, ijk: tuple, exps: np.ndarray, coeffs: np.ndarray, primgauss: np.ndarray) -> None:
-          self.atom = atom_index
-          self.loc = None
-          self.shell = tuple(ijk)
-          self.Nprim = coeffs.shape[0]
-          self.exp = exps
-          self.coef = coeffs
-          self.norm = None
-          self.normalize()
-          self.prim_indices = None
-          self.find_location_and_indices(primgauss)
-          return None
+    Q: can it be sped up? Can we eliminate the need to read in primgauss object?
+    Notes:    
+        1.   It is easy to pass the location array.
+        2.   For any exp in self.exp and i in self.shell, we can use exp and i as keys in dictionary.
+        3.   This would have to change the goal, and we will have to list atom_id = self.atom as a key to be passed to the overlap dictionary. This is because of the following:
+            self.prim_indices contains information on the location already. 
+    """
+    def __init__(self, atom_index: int, ijk: tuple, exps: np.ndarray, coeffs: np.ndarray, primgauss: np.ndarray) -> None:
+        self.atom = atom_index
+        self.loc = None
+        self.shell = tuple(ijk)
+        self.Nprim = coeffs.shape[0]
+        self.exp = exps
+        self.coef = coeffs
+        self.norm = None
+        self.normalize()
+        self.prim_indices = None
+        self.find_location_and_indices(primgauss)
+        return None
 
-     def normalize(self) -> None:
-          '''
-          Routine to normalize the basis functions. Note that s and p orbitals have different normalizations
-          for cartesian and spherical shells, while higher orbitals carry same normalization scheme no matter what.
-          Function modifies attribute:
-               self.norm
-          '''
-          l,m,n = self.shell
-          L = l+m+n
-          if L < 2:
-          # self.norm is a list of length equal to number primitives
-          # normalize primitives first (PGBFs)
-               self.norm = np.sqrt(np.power(2,2*(l+m+n)+1.5)*
-                         np.power(self.exp,l+m+n+1.5)/
-                         fact2(2*l-1)/fact2(2*m-1)/
-                         fact2(2*n-1)/np.power(np.pi,1.5))
-                    # now normalize the contracted basis functions (CGBFs)
-               # Eq. 1.44 of Valeev integral whitepaper
-               prefactor = np.power(np.pi,1.5)*\
-                    fact2(2*l - 1)*fact2(2*m - 1)*fact2(2*n - 1)/np.power(2.0,L)
+    def normalize(self) -> None:
+        """
+        Routine to normalize the basis functions. Note that s and p orbitals have different normalizations
+        for cartesian and spherical shells, while higher orbitals carry same normalization scheme no matter what.
+        Function modifies attribute:
+            self.norm
+        """
+        l,m,n = self.shell
+        L = l+m+n
+        if L < 2:
+            # self.norm is a list of length equal to number primitives
+            # normalize primitives first (PGBFs)
+            self.norm = np.sqrt(np.power(2,2*(l+m+n)+1.5)*
+                        np.power(self.exp,l+m+n+1.5)/
+                        fact2(2*l-1)/fact2(2*m-1)/
+                        fact2(2*n-1)/np.power(np.pi,1.5))
+            # now normalize the contracted basis functions (CGBFs)
+            # Eq. 1.44 of Valeev integral whitepaper
+            prefactor = np.power(np.pi,1.5)*\
+                fact2(2*l - 1)*fact2(2*m - 1)*fact2(2*n - 1)/np.power(2.0,L)
 
-               N = 0.0
-               num_exps = len(self.exp)
-               for ia in range(num_exps):
-                    for ib in range(num_exps):
-                         N += self.norm[ia]*self.norm[ib]*self.coef[ia]*self.coef[ib]/\
-                              np.power(self.exp[ia] + self.exp[ib],L+1.5)
-               N *= prefactor
-               N = N**-.5
-               for ia in range(num_exps):
-                    self.coef[ia] *= N
-          else:
-               self.norm = np.sqrt(np.power(2, L + 2.5)*
-                                   np.power(self.exp, L + 1.5)/
-                                   gamma(1.5 + L))
-          return None
+            N = 0.0
+            num_exps = len(self.exp)
+            for ia in range(num_exps):
+                for ib in range(num_exps):
+                    N += self.norm[ia]*self.norm[ib]*self.coef[ia]*self.coef[ib]/\
+                            np.power(self.exp[ia] + self.exp[ib],L+1.5)
+            N *= prefactor
+            N = N**-.5
+            for ia in range(num_exps):
+                self.coef[ia] *= N
+        else:
+            self.norm = np.sqrt(np.power(2, L + 2.5)*
+                                np.power(self.exp, L + 1.5)/
+                                gamma(1.5 + L))
+        return None
 
-     def find_location_and_indices(self, primgauss: np.ndarray) -> None:
-          """
-          Routine to find location and index in primgauss for each direction.
-          Modifies attributes:
-               self.loc
-               self.prim_indices
-          """
-          self.prim_indices = []
-          cond0 = primgauss[:,0].astype(int) == self.atom
-          self.loc = primgauss[np.where(cond0)[0][0]][4:]
-          for i in self.shell:
-               a = []
-               cond1 = cond0*(primgauss[:,2]==i)
-               for exp in self.exp:
-                    cond2 = cond1*np.isclose(primgauss[:,3], exp)
-                    a.append(np.where(cond2)[0][0])
-               self.prim_indices.append(a)
-          self.prim_indices = np.array(self.prim_indices)
-          return None
+    def find_location_and_indices(self, primgauss: np.ndarray) -> None:
+        """
+        Routine to find location and index in primgauss for each direction.
+        Modifies attributes:
+            self.loc
+            self.prim_indices
+        """
+        self.prim_indices = []
+        cond0 = primgauss[:,0].astype(int) == self.atom
+        self.loc = primgauss[np.where(cond0)[0][0]][4:]
+        for i in self.shell:
+            a = []
+            cond1 = cond0*(primgauss[:,2]==i)
+            for exp in self.exp:
+                cond2 = cond1*np.isclose(primgauss[:,3], exp)
+                a.append(np.where(cond2)[0][0])
+            self.prim_indices.append(a)
+        self.prim_indices = np.array(self.prim_indices)
+        return None
--- dielectric_functions.py
+++ dielectric_functions.py
@@ -2,71 +2,69 @@
 # -*- coding: utf-8 -*-
 
 """
-QCDark branch to calculate the dielectric function:
-     RPA Dielectric function based on atomic centered cartesian gaussian orbitals using PySCF as DFT code.
-     version: 0.01
-     authors: github: @meganhott; github: @asingal14
-     
-Implementation steps to implement next:
-     1. Construct overlap of primitive 1D gaussians, routines.py/calc_ovlp_1D_prim_gauss
-     2. Implement a function to do the density functional theory calculation, and save results to store.
-     ...
-     -1.Implement store:
-          Check if hdf5 file previously exists. 
-          Y. Check if the parameters of calculation have changed.
-               Y. Continue with all previously saved data.
-               N.   1. Check if cell and k-grid parameters are the same.
-                         Y. Check if DFT parameters are the same (Exchange-Correlation)
-                              Y. continue with same DFT.
-                              N. delete DFT related files and parameters if they exist.
-                         N. delete hdf5 file and all data in store, and move to step (-1.N).
-                    2. Delete all pre-existing epsilon results, and molecular orbital overlap files if they exist.
-          N. Create new file.
-               1. Add/update attributes and parameters.
-               2. Make all necessary folders
-          
-"""
+QCDark branch to calculate the dielectric function: 
+    RPA Dielectric function based on atomic centered cartesian gaussian orbitals using PySCF as DFT code.
+    version: 0.01
+    authors: github: @meganhott; github: @asingal14
 
+Implementation steps to implement next:
+    1. Construct overlap of primitive 1D gaussians, routines.py/calc_ovlp_1D_prim_gauss
+    2. Implement a function to do the density functional theory calculation, and save results to store.
+    ...
+    -1.Implement store:
+        Check if hdf5 file previously exists.
+        Y. Check if the parameters of calculation have changed.
+            Y. Continue with all previously saved data.
+            N.  1. Check if cell and k-grid parameters are the same.
+                    Y. Check if DFT parameters are the same (Exchange-Correlation)
+                        Y. continue with same DFT.
+                        N. delete DFT related files and parameters if they exist.
+                    N. delete hdf5 file and all data in store, and move to step (-1.N).
+                2. Delete all pre-existing epsilon results, and molecular orbital overlap files if they exist.
+        N. Create new file.
+            1. Add/update attributes and parameters.
+            2. Make all necessary folders
+"""
+    
 import numpy as np
 import routines as routines
 import input_parameters as parmt
 
 def initialize_cell() -> tuple[routines.pbcgto.cell.Cell, dict]:
-     cell = routines.build_cell_from_input()                                    # Build cell object
-     primgauss = routines.gen_all_1D_prim_gauss(cell)                           # Get all primitive gaussian objects
-     primindices = routines.gen_prim_gauss_indices(primgauss)                   # Get all main indices for primitive gaussian objects. 
-     all_ao = routines.gen_all_atomic_orbitals(cell, primgauss)                 # Get all atomic orbitals
-     G_vectors = routines.gen_G_vectors(cell)                                   # Get all relevant G vectors
-     R_vectors = routines.construct_R_vectors(cell)
-     dark_objects = {
-          'primitive_gaussians': primgauss,
-          'all_ao': all_ao,
-          'G_vectors': G_vectors,
-          'primindices': primindices[0],
-          'atom_locs': primindices[1],
-          'R_vectors': R_vectors
-     }
-     return cell, dark_objects
+    cell = routines.build_cell_from_input()                                    # Build cell object
+    primgauss = routines.gen_all_1D_prim_gauss(cell)                           # Get all primitive gaussian objects
+    primindices = routines.gen_prim_gauss_indices(primgauss)                   # Get all main indices for primitive gaussian objects.
+    all_ao = routines.gen_all_atomic_orbitals(cell, primgauss)                 # Get all atomic orbitals
+    G_vectors = routines.gen_G_vectors(cell)                                   # Get all relevant G vectors
+    R_vectors = routines.construct_R_vectors(cell)
+    dark_objects = {
+        'primitive_gaussians': primgauss,
+        'all_ao': all_ao,
+        'G_vectors': G_vectors,
+        'primindices': primindices[0],
+        'atom_locs': primindices[1],
+        'R_vectors': R_vectors
+    }
+    return cell, dark_objects
 
 def electronic_structure(cell: routines.pbcgto.cell.Cell, dark_objects: dict) -> dict:
-     kmf = routines.KS_electronic_structure(cell)
-     routines.KS_non_self_consistent_field(kmf)
-     routines.convert_to_eV_and_scissor(cell)
-     dark_objects['unique_q'] = routines.get_1BZ_q_points(cell)
-     return dark_objects
+    kmf = routines.KS_electronic_structure(cell)
+    routines.KS_non_self_consistent_field(kmf)
+    routines.convert_to_eV_and_scissor(cell)
+    dark_objects['unique_q'] = routines.get_1BZ_q_points(cell)
+    return dark_objects
 
 def dielectric_RPA(dark_objects: dict) -> None:
-     dark_objects['1d_ovlp'] = routines.primgauss_1D_overlaps(dark_objects)
-     return dark_objects
+    dark_objects['1d_ovlp'] = routines.primgauss_1D_overlaps(dark_objects)
+    return dark_objects
 
 def main():
-     cell, dark_objects = initialize_cell()
-     dark_objects = electronic_structure(cell, dark_objects)
-     dark_objects = dielectric_RPA(dark_objects)
-     return
+    cell, dark_objects = initialize_cell()
+    dark_objects = electronic_structure(cell, dark_objects)
+    dark_objects = dielectric_RPA(dark_objects)
+    return
 
 if __name__ == '__main__':
-     routines.check_requirements()                                              # Check requirements
-     routines.patch()                                                           # Patch required for some versions, 
-                                                                                # see function for details
-     main()
+    routines.check_requirements()                                              # Check requirements
+    routines.patch()                                                           # Patch required for some versions, see function for details
+    main()
