13a9399689aa4cc77fc9532caca442bb019f62d6
meganhott <46502432+meganhott@users.noreply.github.com>
2024-07-29 14:03:23 -0400

    Fixed error in 3D overlaps

--- routines.py
+++ routines.py
@@ -664,11 +664,10 @@ def get_3D_overlaps(q, k_pairs, k_f, mo_coeff_i, mo_coeff_f, dark_objects):
     for G_id,G in enumerate(dark_objects['G_vectors']): #multiprocessing for this step
         #load in relevant q+G 1D overlaps
         qG = q + G
-        q_1d_integrals = []
         Ri_coef_sum = np.zeros_like(R_id)
         for dim in range(3):
             dir = parmt.store + '/primgauss_1d_integrals/dim_{}/'.format(dim)
-            q_1d_integrals.append(np.load(dir+'{:.5f}.npy'.format(qG[dim])))
+            q_1d_integrals = np.load(dir+'{:.5f}.npy'.format(qG[dim]))
 
             coef_sum = np.einsum('ij,kl,mn,iln,jok,jpm->ijop',np.exp(-1j*np.tensordot(unique_Ri[dim],k_f[k_pairs[:,1],dim],0)), np.array([ao_i.all_coef[dim] for ao_i in ao]), np.array([ao_i.all_coef[dim] for ao_i in ao]), q_1d_integrals, mo_coeff_i[k_pairs[:,0],:,:], np.conjugate(mo_coeff_f[k_pairs[:,1],:,:])) #(i,j,k,l,m,n,o,p)=(Rd,k_pair,a,m,b,n,i,j) -> (i,j,o,p)=(Rd,k_pair,i,j)
             #possible einsum optimization with optimize=...
