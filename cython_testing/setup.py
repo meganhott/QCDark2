3b35f8baaa5e43105af5a8a09c63d265e46ad163
meganhott <46502432+meganhott@users.noreply.github.com>
2026-01-22 15:49:15 -0500

    Actually removing development files

--- cython_testing/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(ext_modules=cythonize('cython_testing.pyx', compiler_directives={'language_level': 3}), include_dirs=[np.get_include()])
